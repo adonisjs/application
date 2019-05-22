4a056979651e6d4b7773de24ee018c188168aed3
Harminder virk <virk.officials@gmail.com>
2019-05-22 18:40:40 +0530

    feat: add main module

---
+++ index.ts
@@ -0,0 +1,15 @@
+/*
+* @poppinss/application
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+/// <reference path="./src/contracts.ts" />
+
+export { ApplicationContract } from '@poppinss/application/contracts'
+export { Application } from './src/Application'
+import * as rcParser from './src/rcParser'
+export { rcParser }
