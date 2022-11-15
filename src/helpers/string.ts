e257e7258859860fb2bc67a4119993a0907165a7
Harminder Virk <virk.officials@gmail.com>
2022-11-15 12:07:33 +0530

    feat: add escapeHtml and encodeSymbols helpers

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -7,17 +7,32 @@
  * file that was distributed with this source code.
  */
 
+import he, { EncodeOptions } from 'he'
 import string from '@poppinss/utils/string'
 
 const stringHelpers: typeof string & {
   toSentence: typeof string['sentence']
   ordinalize: typeof string['ordinal']
   generateRandom: typeof string['random']
+  escapeHTML(value: string, options?: { encodeSymbols?: boolean }): string
+  encodeSymbols(value: string, options?: EncodeOptions): string
 } = {
   ...string,
   toSentence: string.sentence,
   ordinalize: string.ordinal,
   generateRandom: string.random,
+
+  escapeHTML(value: string, options?: { encodeSymbols?: boolean }): string {
+    value = he.escape(value)
+    if (options && options.encodeSymbols) {
+      value = this.encodeSymbols(value, { allowUnsafeSymbols: true })
+    }
+    return value
+  },
+
+  encodeSymbols(value: string, options?: EncodeOptions): string {
+    return he.encode(value, options)
+  },
 }
 
 export default stringHelpers
