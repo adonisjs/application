6f1a408876a965876d8264bc88f398da068220e0
Harminder Virk <virk.officials@gmail.com>
2022-11-15 12:17:40 +0530

    test: fix failing tests

--- tests/application/main.spec.ts
+++ tests/application/main.spec.ts
@@ -229,7 +229,7 @@ test.group('Application', (group) => {
 
     await assert.rejects(
       () => app.init(),
-      'The installed Node.js version "v19.0.0" does not satisfy the expected Node.js version "<=10.0.0" defined inside package.json file'
+      `The installed Node.js version "${process.env.version}" does not satisfy the expected Node.js version "<=10.0.0" defined inside package.json file`
     )
   })
 })
